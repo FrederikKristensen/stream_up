ac013b7e6eb8da8e9e5da8667c0849b7f85bfff3
Frederik Kristensen <frede4947@gmail.com>
2025-01-05 17:48:49 +0100

    Menu and api setup

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-alert("The test extension is up and running")
+// alert("The test extension is up and running")
+
+FetchData();
+
+async function fetchdata(){
+    try{
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/gengar");
+
+        if(!response.ok){
+            throw new Error("Could not fetch resource");
+        }
+
+        const data = await response.json();
+        console.log(data);
+
+    }
+    catch(error){
+        console.error(error);
+    }
+}
+
+// https://www.youtube.com/watch?v=37vxWr0WgQk
