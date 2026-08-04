cbea990f9dd83f7671a9d6c0ca256d8737e5882f
Frederik Kristensen <frede4947@gmail.com>
2026-08-04 13:45:09 +0200

    Set up tailwind set up of a few components
    Also added a background color

---
+++ src/components/StreamCards.tsx
@@ -0,0 +1,13 @@
+const StreamCards = () => {
+  return (
+    <div>
+      <p>Thumbnail</p>
+      <h2>Name</h2>
+      <h3>Title</h3>
+      <p>Viewer count</p>
+      <p>Platform icon</p>
+    </div>
+  );
+};
+
+export default StreamCards;
