3aff1e6a51c2f70a4fbc920799c846fbcdf45e30
Frederik Kristensen <frede4947@gmail.com>
2026-09-07 14:58:18 +0200

    More styling of stream cards and a viewer icon

--- src/components/StreamCard.tsx
+++ src/components/StreamCard.tsx
@@ -1,3 +1,4 @@
+import { FaRegEye } from 'react-icons/fa';
 export interface channel {
   user_login: string;
   user_name: string;
@@ -13,11 +14,15 @@ interface StreamCardProps {
 
 const StreamCard = ({ stream }: StreamCardProps) => {
   return (
-    <div className="m-3 p-2 bg-gray-500 rounded-xl">
-      <h1 className="text-gray-100">{stream.title}</h1>
-      <h2 className="text-gray-200">{stream.user_name}</h2>
-      <h3 className="text-gray-400">{stream.game_name}</h3>
-      <p className="text-red-600">{stream.viewer_count}</p>
+    <div className="m-3 p-2 bg-zinc-700 rounded-xl">
+      <h1 className="truncate text-gray-50">{stream.title}</h1>
+      <h2 className="text-gray-200">
+        {stream.user_name} - {stream.game_name}
+      </h2>
+      <div className="text-red-600 flex items-center gap-1">
+        <FaRegEye />
+        <p>{stream.viewer_count}</p>
+      </div>
     </div>
   );
 };
