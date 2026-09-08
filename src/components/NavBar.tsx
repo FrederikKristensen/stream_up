cdf828e2ca65d4358ce41f036f3c7f2944612f74
Frederik Kristensen <frede4947@gmail.com>
2026-09-08 15:07:56 +0200

    Fixed the nav bars position

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,11 +6,15 @@ interface NavBarProps {
 
 const NavBar = ({ TwitchHandle }: NavBarProps) => {
   return (
-    <div className="m-1.5 mb-0.5 p-1 w-full bg-zinc-500 rounded-xl">
-      <button onClick={TwitchHandle} className="text-white">
-        Twitch Login
-      </button>
-      <IoMdSettings size="1.5em" />
+    <div className="m-1.5 mb-0.5 p-2 w-full bg-zinc-500 rounded-xl grid grid-cols-2">
+      <div className="col-span-1">
+        <button onClick={TwitchHandle} className="text-white">
+          Twitch Login
+        </button>
+      </div>
+      <div className="col-span-1 col-end-4 relative right-0">
+        <IoMdSettings size="1.5em" />
+      </div>
     </div>
   );
 };
