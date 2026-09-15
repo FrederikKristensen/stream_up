e22f15b74f86a7cb356ffa76e5d0258b580b0827
Frederik Kristensen <frede4947@gmail.com>
2026-09-15 14:28:19 +0200

    Fixed width
    Turned out that w-full was pushing the width to 386px instead of the assigned 380px

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,7 +10,7 @@ interface NavBarProps {
 const NavBar = ({ TwitchHandle, searchQuery, onSearchChange }: NavBarProps) => {
   return (
     <nav>
-      <div className="relative m-1.5 mb-0 p-2 w-full bg-zinc-500 rounded-xl grid grid-cols-3 items-center">
+      <div className="relative m-1.5 mb-0 p-2 bg-zinc-500 rounded-xl grid grid-cols-3 items-center">
         <div className="flex items-center col-span-1 gap-2">
           <FaSearch color="white" className="shrink-0 size-4" />
           <input
