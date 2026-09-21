c75ce97890b716a17312098192fafa917ae5d05c
Frederik Kristensen <frede4947@gmail.com>
2026-09-21 12:42:07 +0200

    Light mode now have color lighter colors

--- src/components/StreamCard.tsx
+++ src/components/StreamCard.tsx
@@ -36,7 +36,7 @@ const StreamCard = ({ stream }: StreamCardProps) => {
       target="_blank"
       rel="noopener noreferrer"
     >
-      <div className="relative mx-3 my-1.5 bg-card-bg rounded-xl overflow-hidden border-2">
+      <div className="relative mx-2 my-2 bg-card-bg rounded-xl overflow-hidden">
         <div className="relative">
           <img
             src={thumbnailLink}
